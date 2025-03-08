582ed0ff03886b92c72d37fdf4b3973db15ba152
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-08 16:51:54 -0300

    Lesson 04 - Static method

---
+++ aula_04_metodo-estatico/script.js
@@ -0,0 +1,15 @@
+/*
+    Classes
+    - Método estatico
+*/
+
+class User {
+  static showMessage(message) {
+    console.log(message);
+  }
+}
+
+// const user = new User('Essa é uma mensagem');
+// user.showMessage();
+
+User.showMessage('Essa é uma mensagem');
