94636aa157dd13cd71210fc44c52b79d4ee968b1
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-08 16:10:41 -0300

    Lesson 02 - Creating properties within the class

---
+++ aula_02_criando-propriedades-dentro-das-classes/script.js
@@ -0,0 +1,16 @@
+/*
+    Classes
+    - Criando propriedades dentro da classe
+*/
+
+class Product {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const product1 = new Product('Teclado');
+console.log(product1.name);
+
+const product2 = new Product('Mouse');
+console.log(product2.name);
